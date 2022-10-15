d5a526dd953c747416634dbfa8a889be1b335028
Marco Ollivier <marcopollivier@gmail.com>
2022-10-15 13:57:46 -0300

    Create a basic test

--- go-simple-icons_test.go
+++ go-simple-icons_test.go
@@ -4,3 +4,18 @@
  */
 
 package lib
+
+import "testing"
+
+func TestGetIcon(testing *testing.T) {
+	icon, err := GetIcon("Simple Icons")
+
+	if err != nil {
+		testing.Error("Unexpected error")
+	}
+
+	if icon.Title != "Simple Icons" {
+		testing.Error("The Icon name is wrong")
+	}
+
+}
